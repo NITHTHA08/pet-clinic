27ba428125da004d1fc0fc88e3ff013ce2436260
KISHA08 <akishakishani99@gmail.com>
2024-05-25 14:27:49 +0530

    Add files via upload

---
+++ js/admin_script.js
@@ -0,0 +1,52 @@
+
+
+// // add hovered class to selected list item
+// let list = document.querySelectorAll(".navigation li");
+
+// function activeLink() {
+//   list.forEach((item) => {
+//     item.classList.remove("hovered");
+//   });
+//   this.classList.add("hovered");
+// }
+
+// list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// // Menu Toggle
+// let toggle = document.querySelector(".toggle");
+// let navigation = document.querySelector(".navigation");
+// let main = document.querySelector(".main");
+
+// toggle.onclick = function () {
+//   navigation.classList.toggle("active");
+//   main.classList.toggle("active");
+// };
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   navbar.classList.remove('active');
+}
+
+subImages = document.querySelectorAll('.update-product .image-container .sub-images img');
+mainImage = document.querySelector('.update-product .image-container .main-image img');
+
+subImages.forEach(images =>{
+   images.onclick = () =>{
+      let src = images.getAttribute('src');
+      mainImage.src = src;
+   }
+});
